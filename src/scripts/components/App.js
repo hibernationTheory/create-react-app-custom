b2de9f3c798d3b8487bb5a467d440d89fd715946
Engin <hibernationtheory@gmail.com>
2017-05-31 11:57:44 -0400

    add more code to illustrate the api backend use-case

--- src/scripts/components/App.js
+++ src/scripts/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Client from '../utils/Client';
 
 /*
  *  App View Component
@@ -6,11 +7,37 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor(...props) {
     super(...props);
+
+    this.state = {
+      value: 'hello server',
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onChange(evt) {
+    this.setState({
+      value: evt.target.value,
+    });
+  }
+
+  onClick() {
+    const val = this.state.value;
+
+    Client.search(val)
+      .then((result) => {
+        console.log(result);
+      });
   }
 
   render() {
     return (
       <div className="App">
+        <div className="input-field">
+          <input type="text" value={this.state.value} onChange={this.onChange} />
+          <button onClick={this.onClick.bind(this)}>Test API</button>
+        </div>
       </div>
     );
   }
